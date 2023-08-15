62d214e2fb62fc1366c4cef3bdf916dd743860b2
BuildTools <schodllukas8@gmail.com>
2023-08-15 23:20:05 +0200

    installed fonts

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -4,5 +4,14 @@
 <template>
   <main>
     <h1>Hello World</h1>
+    <div class="test-wrapper">
+      <h1>Test Test</h1>
+    </div>
   </main>
 </template>
+
+<style>
+.test-wrapper {
+  font-family: var(--secondary-font);
+}
+</style>
