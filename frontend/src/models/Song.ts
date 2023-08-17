6a9577aaee629f794aae9bce57cee13ea1598d4f
BuildTools <schodllukas8@gmail.com>
2023-08-17 13:28:04 +0200

    created new song fixture and /queue paged request

---
+++ frontend/src/models/Song.ts
@@ -0,0 +1,37 @@
+export interface SongDto {
+    title: string,
+    artist: string
+    duration: number
+}
+
+export class Song {
+    private readonly _title;
+    private readonly _artist;
+    private readonly _duration;
+
+    constructor(title: string, artist: string, duration: number) {
+        this._title = title;
+        this._artist = artist;
+        this._duration = duration;
+    }
+
+    get title() {
+        return this._title;
+    }
+
+    get artist() {
+        return this._artist;
+    }
+
+    get duration() {
+        return this._duration;
+    }
+
+    static fromDto(dto: SongDto): Song {
+        return new Song(
+            dto.title,
+            dto.artist,
+            dto.duration
+        );
+    }
+}
--- frontend/src/server.ts
+++ frontend/src/server.ts
@@ -1,69 +1,47 @@
-import {createServer, Response, Model} from "miragejs"
+import {createServer, Model} from "miragejs"
 import type {Server} from "miragejs/server";
 import type {ModelDefinition, Registry} from "miragejs/-types";
 import type Schema from "miragejs/orm/schema";
-import {User} from "@/models/User";
+import songs from "@/mirage/fixtures/songs";
+import {Song} from "@/models/Song";
 
 // typescript help:
 // https://github.com/miragejs/examples/blob/master/react-typescript/src/mirage/index.ts
 // https://github.com/miragejs/miragejs/issues/460
 
 export function makeServer({environment = "development"} = {}) {
-    const UserModel: ModelDefinition<User> = Model.extend({})
+    const SongModel: ModelDefinition<Song> = Model.extend({});
 
     return createServer({
         environment,
 
         models: {
-            user: UserModel
+            song: SongModel
+        },
+
+        fixtures: {
+            songs
         },
 
         seeds(server: Server) {
-            server.create("user", {
-                username: "Bob",
-                password: "1234",
-                sessionKey: "sdfasdfasdf"
-            } as Object);
-
-            server.create("user", {
-                username: "Alice",
-                password: "35234",
-                sessionKey: "243asdfj"
-            } as Object);
+            server.loadFixtures("songs");
         },
 
         routes() {
             this.namespace = "api"
 
-            type AppRegistry = Registry<{ user: typeof UserModel }, { /* factories can be defined here */ }>
+            type AppRegistry = Registry<{ song: typeof SongModel}, { /* factories can be defined here */ }>
             type AppSchema = Schema<AppRegistry>
 
-            this.get("/users", (schema: AppSchema) => {
-                return schema.all("user")
-            })
-
-            this.post("/login", (schema: AppSchema, request) => {
+            this.post("/queue", (schema: AppSchema, request) => {
                 let requestBody = JSON.parse(request.requestBody);
 
-                const user = schema.findBy("user", {username: requestBody.username});
+                const songs = schema.db.songs;
 
-                if (user === null) {
-                    return new Response(
-                        400,
-                        {},
-                        { errors: ["could not find user"]}
-                    )
-                }
+                let start = requestBody.pageSize * requestBody.pageNumber;
+                let end = start + requestBody.pageSize;
 
-                if (user.password !== requestBody.password) {
-                    return new Response(
-                        400,
-                        {},
-                        { errors: ["wrong password"]}
-                    )
-                }
-
-                return {sessionKey: user.sessionKey};
+                return songs.slice(start, end);
             })
         },
     })
