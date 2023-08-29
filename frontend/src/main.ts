a2241abbe192bc770265a11452b0744a8f0853f9
BuildTools <schodllukas8@gmail.com>
2023-08-29 10:56:31 +0200

    moved server.ts to /mirage

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,7 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import {makeServer} from "@/server";
+import {makeServer} from "@/mirage/server";
 
 if (import.meta.env.DEV) {
     makeServer();
