1dbb6ba956e43c3b167dcbdce802605401037167
Quantompixel <39237785+Quantompixel@users.noreply.github.com>
2023-09-08 10:57:51 +0200

    created HomeView and renamed old HomeView to PublicView

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,30 +1,11 @@
 <template>
-  <main>
-    <Collapse :label="$translate('nowPlaying')" :is-collapsed="false">
-      <NowPlaying :update-interval="1000"/>
-    </Collapse>
-    <Collapse :label="$translate('inQueue')" :is-collapsed="false">
-      <Queue :page-update-interval="4000"/>
-    </Collapse>
-  </main>
+
 </template>
 
 <script setup lang="ts">
-import Queue from "@/components/queue/Queue.vue";
-import Collapse from "@/components/Collapse.vue";
-import NowPlaying from "@/components/NowPlaying.vue";
+
 </script>
 
-<style>
-main {
-  padding-top: 20px;
-  width: 90%;
-  margin: auto;
-}
+<style scoped>
 
-@media screen and (min-width: 800px) {
-  main {
-    width: 750px;
-  }
-}
 </style>
