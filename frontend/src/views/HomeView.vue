88145444123aff7cb7ed358f5c92c730690f9a72
Quantompixel <39237785+Quantompixel@users.noreply.github.com>
2023-09-08 15:09:03 +0200

    work on voteskip

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,5 +1,9 @@
 <template>
-
+<div class="wrapper">
+  <div class="button"></div>
+  <div class="slider"></div>
+  <div class="label">4/5</div>
+</div>
 </template>
 
 <script setup lang="ts">
@@ -7,5 +11,25 @@
 </script>
 
 <style scoped>
+.wrapper {
+  height: 70px;
+}
 
+.button {
+  position: relative;
+  z-index: 1;
+  top: 25px;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  background: blue;
+}
+
+.slider {
+  position: relative;
+  width: 100px;
+  height: 20px;
+  background: red;
+  bottom: calc(35px - 10px);
+}
 </style>
