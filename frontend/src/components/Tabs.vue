0a0cc8b6785d98cece1e2f32c019455c32f487f3
BuildTools <schodllukas8@gmail.com>
2023-09-11 21:48:04 +0200

    Tabs.vue component functionally finished

---
+++ frontend/src/components/Tabs.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="tab-switcher-wrapper">
+    <div class="tabs-container">
+      <div v-for="(tab, index) in tabs"
+           :key="index"
+           @click="setActiveTab(index)"
+           class="tab unselectable"
+           :class="[index === activeTab ? 'active' : 'inactive']">
+        {{ tab }}
+      </div>
+      <div class="spacer"></div>
+    </div>
+    <div class="content-wrapper">
+      <div v-for="(tab, index) in tabs"
+           :key="index"
+           class="tab-wrapper"
+           v-show="index === activeTab">
+        <slot :name="`tab-${index}`"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {ref} from "vue";
+
+const tabs = ref(["test1", "test2"])
+const activeTab = ref(0);
+
+function setActiveTab(index: number) {
+  if (index < 0 || index > tabs.value.length - 1) {
+    return;
+  }
+
+  activeTab.value = index;
+}
+</script>
+
+
+<style scoped>
+
+.tab-switcher-wrapper {
+  width: calc(100% - 40px);
+  height: 100px;
+}
+
+.tabs-container {
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+  gap: 10px;
+}
+
+.tab {
+  width: 100%;
+  height: 40px;
+  border-radius: var(--border-radius-medium) var(--border-radius-medium) 0 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-sizing: border-box;
+  cursor: pointer;
+  border: var(--tertiary-color) 2px solid;
+  border-bottom: none;
+  box-sizing: border-box;
+}
+
+.tab.active {
+  background: var(--tertiary-color);
+  color: var(--background-color);
+}
+
+.tab.inactive {
+ background: none;
+  color: var(--text-color);
+}
+
+.spacer {
+  display: none;
+}
+
+.content-wrapper {
+  border: var(--tertiary-color) 5px solid;
+  border-radius: 0 0 var(--border-radius-medium) var(--border-radius-medium);
+  height: 100%;
+}
+
+.tab-wrapper.active {
+  display: block;
+}
+
+.tab-wrapper.inactive {
+  display: none;
+}
+
+@media screen and (min-width: 420px) {
+  .tab {
+    width: 160px;
+  }
+
+  .spacer {
+    display: block;
+    width: calc(100% - 320px);
+  }
+
+  .content-wrapper {
+    border-radius: 0 var(--border-radius-medium) var(--border-radius-medium) var(--border-radius-medium);
+  }
+}
+
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,13 +1,14 @@
 <template>
   <main>
     <OverlayCollapse label="Add Song" :icon="resolveFilePath('/icons/music/playlist_add.svg')" :is-collapsed="false">
-      <div class="tab-switcher-wrapper">
-        <div class="tabs-container">
-          <div class="tab">tab1</div>
-          <div class="tab">tab2</div>
-          <div class="spacer"></div>
-        </div>
-      </div>
+      <Tabs>
+        <template #tab-0>
+          <p>hallo</p>
+        </template>
+        <template #tab-1>
+          <p>hallo2</p>
+        </template>
+      </Tabs>
     </OverlayCollapse>
     <VoteSkip :update-interval="4000"/>
   </main>
@@ -17,30 +18,7 @@
 import VoteSkip from "@/components/control/VoteSkip.vue";
 import OverlayCollapse from "@/components/collapse/OverlayCollapse.vue";
 import {resolveFilePath} from "@/services/urlService";
+import Tabs from "@/components/Tabs.vue";
 </script>
 
-<style scoped>
-.tab-switcher-wrapper {
-  width: calc(100% - 40px);
-  height: 100px;
-  background: red;
-}
-
-.tabs-container {
-  display: flex;
-  justify-content: space-around;
-  width: 100%;
-}
-
-.tab {
-  background: blue;
-  width: 40%;
-}
-
-@media screen and (min-width: 420px) {
-  .tab {
-    background: blue;
-    width: 210px;
-  }
-}
-</style>
+<style scoped></style>
