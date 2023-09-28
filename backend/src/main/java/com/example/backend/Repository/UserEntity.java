85ba694f7469721d51f7971cadc8bff4f2a58a9c
BuildTools <schodllukas8@gmail.com>
2023-09-28 22:56:06 +0200

    created UserEntity and UserService

---
+++ backend/src/main/java/com/example/backend/Repository/UserEntity.java
@@ -0,0 +1,62 @@
+package com.example.backend.Repository;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "UserEntity")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "token")
+    private String token;
+
+    public UserEntity() {
+
+    }
+
+    public UserEntity(String username, String password, String token) {
+        this.username = username;
+        this.password = password;
+        this.token = token;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
---
+++ backend/src/main/java/com/example/backend/user_management/UserService.java
@@ -0,0 +1,7 @@
+package com.example.backend.user_management;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
