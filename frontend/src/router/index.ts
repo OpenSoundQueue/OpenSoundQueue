116021da1d40c2eb2a840bbf5e71262ea4124ef9
Daniel Pillwein <9132@htl.rennweg.at>
2023-10-01 19:15:14 +0200

    setup done

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,27 +1,33 @@
-import {createRouter, createWebHistory} from "vue-router"
-import PublicView from "@/views/PublicView.vue"
+import { createRouter, createWebHistory } from 'vue-router'
+import PublicView from '@/views/PublicView.vue'
 import HomeView from "@/views/HomeView.vue";
+import LoginView from "@/views/LoginView.vue";
 import SettingsView from "@/views/SettingsView.vue";
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: "/",
-            name: "public",
-            component: PublicView
-        },
-        {
-            path: "/home",
-            name: "home",
-            component: HomeView
-        },
-        {
-            path: "/settings",
-            name: "settings",
-            component: SettingsView
-        }
-    ]
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'public',
+      component: PublicView
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView
+    },
+    {
+      path: "/settings",
+      name: "settings",
+      component: SettingsView
+    }
+  ]
 })
 
 export default router
