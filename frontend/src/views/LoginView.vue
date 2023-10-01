18f47546b1969c66882c2c5c18e4c4d5a294526e
Daniel Pillwein <9132@htl.rennweg.at>
2023-10-01 19:15:14 +0200

    setup done

---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,15 @@
+<template>
+<header>
+  <router-link to="/">
+    <img src="@/assets/icons/arrows/keyboard_arrow_left.svg">
+  </router-link>
+</header>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
