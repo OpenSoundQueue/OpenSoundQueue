fcede23bb3a7d3306e9f2cc05a903708101d0961
Daniel Pillwein <9132@htl.rennweg.at>
2023-10-03 13:22:29 +0200

    started implementing settings

---
+++ backend/src/main/java/com/example/backend/settings_management/SettingsService.java
@@ -0,0 +1,19 @@
+package com.example.backend.settings_management;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SettingsService {
+
+    @Value("${room.public}")
+    private Boolean isPublic;
+
+    @Value("$room.code}")
+    private String roomCode;
+
+    public void printSettings() {
+        //System.out.println("is public: " + isPublic);
+        //System.out.println("roomCode: " + roomCode);
+    }
+}
