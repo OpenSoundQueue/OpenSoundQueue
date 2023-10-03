7f9bce929bd4ae87c292d9c2227b78a7d5919320
BuildTools <schodllukas8@gmail.com>
2023-10-03 16:33:39 +0200

    implemented v-model

--- frontend/src/components/inputs/Select.vue
+++ frontend/src/components/inputs/Select.vue
@@ -2,7 +2,7 @@
   <div class="select-wrapper">
     <label class="input-field-label">{{ label }} <span v-if="required">*</span></label>
     <div class="select-container" @click="isActive = !isActive">
-      <img class="drop-down-icon"
+      <img class="drop-down-icon" alt="Dropdown Icon"
            src="@/assets/icons/arrows/keyboard_arrow_down.svg"
            :style="{transform: `rotate(${isActive ? 180 : 0}deg)`}"
       />
