309805bdec0b0e3a1aa805d9887e3ccc93bfea6c
Markus Wizany <9143@htl.rennweg.at>
2023-10-07 20:58:42 +0200

    api definition

--- backend/src/main/java/com/example/backend/settings_management/SettingsService.java
+++ backend/src/main/java/com/example/backend/settings_management/SettingsService.java
@@ -6,11 +6,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class SettingsService {
 
-    @Value("${room.public}")
+    /*@Value("${room.public}")
     private Boolean isPublic;
 
     @Value("$room.code}")
-    private String roomCode;
+    private String roomCode;*/
 
     public void printSettings() {
         //System.out.println("is public: " + isPublic);
