1534b0f46bb34463622562c6112d81ce36440cb5
Daniel Pillwein <9132@htl.rennweg.at>
2023-10-09 17:20:09 +0200

    ValidationHelper -> ValidationPlugin

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,6 +5,7 @@ import App from './App.vue'
 import router from './router'
 import {makeServer} from "@/mirage/server";
 import {TranslationPlugin} from "@/plugins/TranslationPlugin";
+import {ValidationPlugin} from "@/plugins/ValidationPlugin";
 import translations from "@/translations/en.json";
 
 if (import.meta.env.DEV) {
@@ -16,5 +17,7 @@ const app = createApp(App)
 app.use(router)
 
 app.use(TranslationPlugin, translations);
+
+app.use(ValidationPlugin)
 
 app.mount('#app')
