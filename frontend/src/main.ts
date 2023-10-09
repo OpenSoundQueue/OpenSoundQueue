e5b7a86aadf2ac7568c122cf89b5885687c0ebb4
Daniel Pillwein <9132@htl.rennweg.at>
2023-10-09 21:54:25 +0200

    added ValidationPlugin

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,6 +6,7 @@ import router from './router'
 import {makeServer} from "@/mirage/server";
 import {TranslationPlugin} from "@/plugins/TranslationPlugin";
 import translations from "@/translations/en.json";
+import {ValidationPlugin} from "@/plugins/ValidationPlugin";
 
 if (import.meta.env.DEV) {
     makeServer();
@@ -16,5 +17,7 @@ const app = createApp(App)
 app.use(router)
 
 app.use(TranslationPlugin, translations);
+
+app.use(ValidationPlugin)
 
 app.mount('#app')
