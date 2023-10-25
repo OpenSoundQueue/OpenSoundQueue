32db06d765eb95f134fac80f0e56ec7584d0c9a8
BuildTools <schodllukas8@gmail.com>
2023-10-25 17:36:51 +0200

    first iteration of grid layout on HomeView.vue

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -44,12 +44,11 @@ main {
 
 .queue-scroll-container {
   border-bottom: var(--secondary-color) dashed 2px;
-  height: calc(100% - 70px - 230px);
+  height: calc(100% - 70px - 190px);
 }
 
 .control-panel-wrapper {
   width: 100%;
-  height: 230px;
   bottom: 0;
   left: 0;
 }
@@ -57,30 +56,79 @@ main {
 .control-panel-container {
   margin: auto;
   background: var(--background-color);
+  height: 190px;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  padding: 0 20px;
   box-sizing: border-box;
 }
 
-.now-playing-container {
-  margin-top: 20px;
-}
-
 .vote-skip-container {
-  margin: 20px 0;
+  margin: 10px 0;
   display: flex;
   justify-content: center;
   align-items: flex-start;
 }
 
-@media screen and (min-width: 800px){
+@media screen and (min-width: 800px) {
   main {
     width: 750px;
   }
-  .control-panel-container {
-    width: 750px;
+}
+
+@media screen and (min-width: 1100px){
+  main {
+    width: 1100px;
+    display: grid;
+    grid-template-columns: 66% 33%;
+    grid-template-rows: calc(100% - 230px) 230px;
   }
+
+  .add-song-container {
+    grid-column: 2;
+    grid-row: 1;
+  }
+
+  .queue-scroll-container {
+    grid-column: 1;
+    grid-row: 1;
+    height: 100%;
+    border-bottom: none;
+  }
+
+  .control-panel-wrapper {
+    grid-column: 1 / span 2;
+    grid-row: 2;
+    height: 100%;
+    display: flex;
+    align-items: center;
+  }
+
+  .control-panel-container {
+    width: 100%;
+    border: var(--secondary-color) 3px solid;
+    border-radius: var(--border-radius-big);
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: row;
+  }
+
+  .now-playing-container {
+    width: 66%;
+    padding: 20px;
+    box-sizing: border-box;
+    border-right: red solid 1px;
+    display: grid;
+    place-items: center;
+  }
+
+  .vote-skip-container {
+    border-left: green solid 1px;
+    width: 33%;
+    display: grid;
+    place-items: center;
+    margin: 0;
+  }
+
 }
 </style>
