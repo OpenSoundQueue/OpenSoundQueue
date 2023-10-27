5494125ba879ce6019946c6a4169a989494285ea
BuildTools <schodllukas8@gmail.com>
2023-10-27 10:08:52 +0200

    switching between mobile and desktop menu works

---
+++ frontend/src/components/Link.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="link-wrapper">
+    <div v-if="isExternal" class="external-container">
+      <a :href="path" target="_blank" class="link">{{ label}}</a>
+      <img class="icon-external" src="@/assets/icons/arrows/arrow_outward.svg"/>
+    </div>
+    <div v-else>
+      <router-link :to="path" class="link" :class="{'outer-area': isOuterArea}">
+        {{ label }}
+        <div class="underline"></div>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  isExternal?: boolean,
+  isOuterArea?: boolean,
+  path: string,
+  label: string
+}>();
+</script>
+
+
+<style scoped>
+.link {
+  text-decoration: none;
+  color: var(--text-color);
+  font-size: var(--font-size-nav);
+}
+
+.router-link-active {
+  .underline {
+    background: var(--primary-color);
+    margin-top: 4px;
+    height: 3px;
+    width: 100%;
+  }
+}
+
+.external-container {
+  display: flex;
+  align-items: center;
+}
+
+.outer-area {
+  color: var(--tertiary-color);
+}
+
+.icon-external {
+  height: 100%;
+}
+</style>
--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,14 +1,49 @@
 <template>
   <div class="navbar-wrapper">
     <nav class="navbar">
-      <router-link to="/">
-        <img class="logo" src="@/assets/menu/logo.svg">
-      </router-link>
+      <div @click="toggleMenu" class="open-button mobile">
+        <div>
+          <img src="@/assets/icons/burger_menu.svg"/>
+        </div>
+      </div>
+      <div class="logo-container">
+        <router-link to="/">
+          <img class="logo" src="@/assets/menu/logo.svg">
+        </router-link>
+      </div>
+      <div class="menu" :class="{opened: menuIsOpen, closed: !menuIsOpen}">
+        <div @click="toggleMenu" class="close-button mobile">
+          <img src="@/assets/icons/input/delete.svg"/>
+        </div>
+        <div class="links">
+          <div class="quick-nav-links">
+            <Link path="/" label="Public" :is-outer-area="true"/>
+            <Link path="/settings" label="Settings"/>
+            <Link path="/home" label="Home"/>
+            <Link path="/admin" label="Admin"/>
+          </div>
+          <div class="other-nav-links">
+            <Link path="https://opensoundqueue.org/team" label="Team" :is-external="true"/>
+            <Link path="https://opensoundqueue.org/team" label="About" :is-external="true"/>
+          </div>
+        </div>
+      </div>
+      <div class="user-button">
+        <img src="@/assets/icons/user.svg"/>
+      </div>
     </nav>
   </div>
 </template>
 
 <script setup lang="ts">
+import Link from "@/components/Link.vue";
+import {ref} from "vue";
+
+const menuIsOpen = ref(false);
+
+function toggleMenu() {
+  menuIsOpen.value = !menuIsOpen.value;
+}
 </script>
 
 <style scoped>
@@ -19,7 +54,7 @@
   width: 100%;
   border-bottom: 2px var(--dark-gray) solid;
   z-index: 2;
-  background: var(--background-color-transparent);
+  background: var(--background-color);
   display: flex;
 }
 
@@ -32,6 +67,58 @@
   justify-content: space-between;
 }
 
+.open-button img {
+  height: 15px;
+}
+
+.user-button img {
+  height: 30px;
+}
+
+.close-button {
+  height: 60px;
+  width: 60px;
+  display: grid;
+  place-items: center;
+}
+
+.close-button img {
+  height: 35px;
+}
+
+.menu {
+  position: fixed;
+  width: 66%;
+  left: 0;
+  top: 0;
+  height: calc(100% - 10px);
+  margin-top: 5px;
+  background: var(--secondary-color);
+  border: 2px solid var(--tertiary-color);
+  border-left: none;
+  box-sizing: border-box;
+  border-radius: 0 var(--border-radius-big) var(--border-radius-big) 0;
+  transition: 0.1s transform ease-in;
+}
+
+.menu.closed {
+  transform: translateX(-100%);
+}
+
+.links {
+  height: 100%;
+  margin-left: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.quick-nav-links, .other-nav-links {
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+}
+
 .logo {
   height: 30px;
 }
@@ -41,9 +128,35 @@
   margin: 0;
 }
 
-@media screen and (min-width: 1250px) {
+@media screen and (min-width: 800px) {
+  .mobile {
+    display: none;
+  }
+
   .navbar {
-    width: 1250px;
+    gap: 30px;
+  }
+
+  .menu {
+    position: static;
+    background: none;
+    border: none;
+    height: fit-content;
+    width: 100%;
+    flex-direction: row;
+  }
+
+  .menu.closed {
+    transform: none;
+  }
+
+  .links {
+    flex-direction: row;
+    margin: 0;
+  }
+
+  .quick-nav-links, .other-nav-links {
+    flex-direction: row;
   }
 }
 </style>
