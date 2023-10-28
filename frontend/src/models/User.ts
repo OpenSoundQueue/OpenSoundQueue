5257984a206749f111f0d6a367e78fc95975808c
Daniel Pillwein <9132@htl.rennweg.at>
2023-10-28 12:47:54 +0200

    added id to user model and fixtures

--- frontend/src/models/User.ts
+++ frontend/src/models/User.ts
@@ -1,17 +1,22 @@
 export class User {
+    private readonly _id: number;
     private readonly _username: string;
     private readonly _lastOnline: Date;
     private readonly _email: string;
     private readonly _role: string;
 
-    constructor(username: string, lastOnline: Date, email: string, role: string) {
+    constructor(id: number, username: string, lastOnline: Date, email: string, role: string) {
+        this._id = id;
         this._username = username;
         this._lastOnline = lastOnline;
         this._email = email;
         this._role = role;
     }
 
-    get getUsername(): string {
+    get id(): number {
+        return this._id;
+    }
+    get username(): string {
         return this._username;
     }
 
