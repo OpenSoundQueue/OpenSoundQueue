7a3b774e2aa2eb52ef98f7b0db9ef9485db4e30c
Daniel Pillwein <9132@htl.rennweg.at>
2023-11-09 11:25:13 +0100

    added spam protection

--- frontend/src/components/control/ReplaySong.vue
+++ frontend/src/components/control/ReplaySong.vue
@@ -1,7 +1,7 @@
 <template>
-<div class="replay" @click="replaySong">
-  <img src="@/assets/icons/arrows/replay.svg" alt="skip icon">
-</div>
+  <div class="replay" @click="isLoading?{}:replaySong()" :class="isClicked?'rotate':''">
+      <img src="@/assets/icons/arrows/replay.svg" alt="skip icon">
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -9,23 +9,56 @@ import * as cookieService from "@/services/cookieService";
 import {ToastService} from "@/services/ToastService";
 import {translate} from "@/plugins/TranslationPlugin";
 import {HttpService} from "@/services/HttpService";
+import {ref} from "vue";
 
 const httpService = new HttpService();
+const isClicked = ref(false);
+const isLoading = ref(false);
+
 async function replaySong() {
+  isClicked.value = true;
+  isLoading.value = true;
   await httpService.postReplay(cookieService.getApiKey())
       .catch(() => {
         ToastService.sendNotification(translate("notifications.replaySongError"), "error", 3000);
       });
+  await new Promise((resolve) => setTimeout(resolve, 350));
+  isClicked.value = false;
+  isLoading.value = false;
 }
 </script>
 
 <style scoped>
 
-.replay, img {
-  width: 27px;
+.replay {
+  display: flex;
+  align-items: center;
 }
 
-.replay:hover {
+img {
+  width: 27px;
+  transform: scale(-1, 1);
+}
+
+.replay:hover  {
   cursor: pointer;
+}
+
+.rotate {
+  animation: rotate 0.35s ease-in-out;
+}
+
+@keyframes rotate {
+  0% {
+    transform: rotate(0);
+  }
+
+  50% {
+    transform: rotate(-90deg);
+  }
+
+  100% {
+    transform: rotate(0);
+  }
 }
 </style>
--- frontend/src/components/control/SkipSong.vue
+++ frontend/src/components/control/SkipSong.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="skip-song" @click="skipSong">
+  <div class="skip-song" @click="isLoading?{}:skipSong()" :class="isClicked?'animate':''">
     <img src="@/assets/icons/music/skip.svg" alt="skip icon">
   </div>
 </template>
@@ -9,10 +9,15 @@ import * as cookieService from "@/services/cookieService";
 import {ToastService} from "@/services/ToastService";
 import {translate} from "@/plugins/TranslationPlugin";
 import {HttpService} from "@/services/HttpService";
+import {ref} from "vue";
 
 const httpService = new HttpService();
+const isClicked = ref(false);
+const isLoading = ref(false);
 
 async function skipSong() {
+  isClicked.value = true;
+  isLoading.value = true;
   await httpService.postSkip(cookieService.getApiKey())
       .then((data: []) => {
         ToastService.sendNotification(translate("notifications.skipSongSuccess"), "success", 3000);
@@ -20,15 +25,42 @@ async function skipSong() {
       .catch(() => {
         ToastService.sendNotification(translate("notifications.skipSongError"), "error", 3000);
       });
+  await new Promise((resolve) => setTimeout(resolve, 200));
+  isClicked.value = false;
+  isLoading.value = false;
 }
 </script>
 
 <style scoped>
-.skip-song, img {
+
+.skip-song {
+  display: flex;
+  align-items: center;
+}
+
+img {
   width: 15px;
 }
 
 .skip-song:hover {
   cursor: pointer;
+}
+
+.animate {
+  animation: shrink 0.2s ease-in-out;
+}
+
+@keyframes shrink {
+  0% {
+    transform: scale(1);
+  }
+
+  25% {
+    transform: scale(0.75);
+  }
+
+  100% {
+    transform: scale(1);
+  }
 }
 </style>
