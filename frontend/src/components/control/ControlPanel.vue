397439ca5bf2121bed12af093b818b2d5c4289ee
Daniel Pillwein <9132@htl.rennweg.at>
2023-11-09 14:06:35 +0100

    added StartStopSong component

--- frontend/src/components/control/ControlPanel.vue
+++ frontend/src/components/control/ControlPanel.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="control-panel">
     <ReplaySong v-show="replay"/>
-    <div v-show="voteSkip" class="vote-skip">
+    <div class="skip-container">
+          <div v-show="voteSkip" class="vote-skip">
       <VoteSkip :update-interval="4000"/>
       <InfoButton>{{ $translate('help.voteSkip') }}</InfoButton>
+    </div>
+    <StartStopSong v-show="skip"/>
     </div>
     <SkipSong v-show="skip"/>
   </div>
@@ -14,6 +17,8 @@ import VoteSkip from "@/components/control/VoteSkip.vue";
 import InfoButton from "@/components/InfoButton.vue";
 import SkipSong from "@/components/control/SkipSong.vue";
 import ReplaySong from "@/components/control/ReplaySong.vue";
+import StartStopSong from "@/components/control/StartStopSong.vue";
+
 const props = defineProps<{
   voteSkip:boolean,
   startStop:boolean,
@@ -33,6 +38,14 @@ const props = defineProps<{
 .control-panel{
   gap: 30px;
   width: 100%;
+}
+
+.skip-container{
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  justify-content: center;
+  align-items: center;
 }
 
 .vote-skip{
---
+++ frontend/src/components/control/StartStopSong.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="skip-song" @click="isLoading?{}:startStop()" :class="isClicked?'animate':''">
+    <img class="play-icon" v-show="!isPlaying" src="@/assets/icons/music/play.svg" alt="skip icon">
+    <img class="stop-icon" v-show="isPlaying" src="@/assets/icons/music/pause.svg" alt="skip icon">
+  </div>
+</template>
+
+<script setup lang="ts">
+import * as cookieService from "@/services/cookieService";
+import {ToastService} from "@/services/ToastService";
+import {translate} from "@/plugins/TranslationPlugin";
+import {HttpService} from "@/services/HttpService";
+import {ref} from "vue";
+
+const httpService = new HttpService();
+const isClicked = ref(false);
+const isLoading = ref(false);
+const isPlaying = ref(true);
+
+async function startStop() {
+  isClicked.value = true;
+  isLoading.value = true;
+  if (isPlaying.value){
+    await httpService.postStop(cookieService.getApiKey())
+        .then((data: []) => {
+          isPlaying.value = false;
+        })
+        .catch(() => {
+          ToastService.sendNotification(translate("notifications.stopError"), "error", 3000);
+        });
+  }else {
+    await httpService.postStart(cookieService.getApiKey())
+        .then((data: []) => {
+          isPlaying.value = true;
+        })
+        .catch(() => {
+          ToastService.sendNotification(translate("notifications.startError"), "error", 3000);
+        });
+  }
+
+  await new Promise((resolve) => setTimeout(resolve, 200));
+  isClicked.value = false;
+  isLoading.value = false;
+}
+</script>
+
+<style scoped>
+
+.skip-song {
+  position: relative;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background: var(--primary-color);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition-property: left;
+  transition-duration: .2s;
+}
+
+img {
+  width: 15px;
+}
+
+.play-icon{
+  padding-left: 3px;
+}
+
+.stop-icon{
+  width: 12px;
+}
+
+.skip-song:hover {
+  cursor: pointer;
+}
+
+.animate {
+  animation: shrink 0.2s ease-in-out;
+}
+
+@keyframes shrink {
+  0% {
+    transform: scale(1);
+  }
+
+  25% {
+    transform: scale(0.75);
+  }
+
+  100% {
+    transform: scale(1);
+  }
+}
+</style>
