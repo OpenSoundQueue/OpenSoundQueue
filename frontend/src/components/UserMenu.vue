15b7f0fbcc509a0522ccbe8b4e71089145f1aa13
BuildTools <schodllukas8@gmail.com>
2023-11-19 19:35:45 +0100

    completed user menu layout

---
+++ frontend/src/components/UserMenu.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="user-menu-wrapper">
+    <div class="user-menu-container">
+      <div class="close-container">
+        <img @click="close" src="@/assets/menu/close.svg"/>
+      </div>
+      <p class="username">Quantompixel</p>
+      <p class="email">quantompxiel@adfa.com</p>
+      <p class="role"><span class="dot"></span>Test</p>
+      <div class="button">
+        <DefaultButton text="Logout"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import DefaultButton from "@/components/buttons/DefaultButton.vue";
+
+const emit = defineEmits<{
+  close: []
+}>();
+
+function close() {
+  console.log("ehlo");
+  emit("close");
+}
+</script>
+
+<style scoped>
+.user-menu-wrapper {
+  position: fixed;
+  width: 100%;
+  right: 0;
+  top: 0;
+  padding: 5px;
+  box-sizing: border-box;
+}
+
+.user-menu-container {
+  background: var(--secondary-color);
+  border: 2px solid var(--tertiary-color);
+  border-radius: var(--border-radius-big);
+  display: flex;
+  flex-direction: column;
+  padding: 10px 20px 20px 20px;
+  box-sizing: border-box;
+}
+
+.close-container {
+  display: flex;
+  justify-content: flex-end;
+}
+
+.username, .email, .role {
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  margin: 5px 0;
+}
+
+.dot {
+  height: 5px;
+  aspect-ratio: 1/1;
+  background-color: var(--tertiary-color);
+  border-radius: 50%;
+  display: inline-block;
+  margin-right: 5px;
+}
+
+.role {
+  font-size: var(--font-size-small);
+  background-color: var(--background-color);
+  width: fit-content;
+  max-width: 20%;
+  box-sizing: border-box;
+  padding: 5px;
+  border-radius: var(--border-radius-small);
+  text-align: left;
+}
+
+.button {
+  margin-top: 15px;
+}
+
+@media screen and (min-width: 400px)  {
+  .user-menu-wrapper {
+    width: 400px;
+  }
+}
+</style>
