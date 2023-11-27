a18f50eabfeb04e507217ad84d4333eda8810c21
Markus Wizany <9143@htl.rennweg.at>
2023-11-27 11:58:17 +0100

    email address changed to opensoundqueue domain

--- backend/src/main/java/com/example/backend/email/EmailComponent.java
+++ backend/src/main/java/com/example/backend/email/EmailComponent.java
@@ -2,6 +2,7 @@ package com.example.backend.email;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.mail.*;
@@ -17,6 +18,9 @@ import java.util.Properties;
 @Component
 public class EmailComponent {
     private static final Logger LOG = LoggerFactory.getLogger(EmailComponent.class);
+
+    @Value("${email.password}")
+    private String password;
 
     public void sendMail(String email, String verificationCode, String username) throws MessagingException, IOException {
         MimeMessage message = createMailTemplate(email);
@@ -36,20 +40,22 @@ public class EmailComponent {
         LOG.info("Email wurde versendet!");
     }
 
-    private static MimeMessage createMailTemplate(String email) throws MessagingException {
+    private MimeMessage createMailTemplate(String email) throws MessagingException {
         String to = email;
-        String from = "cityquest.noreply@gmail.com";
-        String host = "smtp.gmail.com";
+        String from = "registration@opensoundqueue.org";
+        String host = "smtp.easyname.eu";
         Properties properties = System.getProperties();
         properties.put("mail.smtp.host", host);
         properties.put("mail.smtp.port", "465");
         properties.put("mail.smtp.ssl.enable", "true");
+        properties.put("mail.smtp.socketFactory.class",
+                "javax.net.ssl.SSLSocketFactory");
         properties.put("mail.smtp.auth", "true");
         properties.put("mail.debug", false);
 
         Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("cityquest.noreply@gmail.com", "lwykmklxlaykjufg");
+                return new PasswordAuthentication("registration@opensoundqueue.org", password);
             }
         });
         MimeMessage message = new MimeMessage(session);
