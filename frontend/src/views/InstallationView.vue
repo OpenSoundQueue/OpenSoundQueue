a1f3bfa041e9f0d393a3e1b1969cbd6f399d1793
Daniel <9132@htl.rennweg.at>
2023-12-29 13:41:27 +0100

    implemented buttons and moved progress bar

--- frontend/src/views/InstallationView.vue
+++ frontend/src/views/InstallationView.vue
@@ -1,19 +1,29 @@
 <template>
+  <nav>
+    <InstallationProgress :installation-order="installationSteps"/>
+  </nav>
   <main>
     <header>
       <img class="header-image" src="@/assets/logo/logo_white.svg" :alt="$translate('altTexts.logo')">
     </header>
     <div class="install-container">
-      <InstallationProgress
-          :installation-order="['language', 'registration','privacy', 'sources']"/>
-      <br><br>
       <component :is="component"/>
     </div>
   </main>
+  <div class="button-container">
+    <div v-if="router.currentRoute.value.name!='language'" class="button back" @click="back()">
+      <div>{{ translate(lastStep) }}</div>
+      <img src="@/assets/icons/arrows/keyboard_arrow_left.svg" :alt="$translate('altTexts.arrowLeft')">
+    </div>
+    <div class="button next" :class="[router.currentRoute.value.name=='language'? 'full-width':'']" @click="next()">
+      <div>{{ translate(nextStep) }}</div>
+      <img src="@/assets/icons/arrows/keyboard_arrow_right.svg" :alt="$translate('altTexts.arrowRight')">
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import {onMounted, shallowRef, watch} from "vue";
+import {computed, onMounted, shallowRef, watch} from "vue";
 import type {Component, ShallowRef} from "vue";
 import {HttpService} from "@/services/HttpService";
 import LanguageSetting from "@/components/installation/LanguageSetting.vue";
@@ -21,9 +31,19 @@ import PrivacySetting from "@/components/installation/PrivacySetting.vue";
 import SourceSetting from "@/components/installation/SourceSetting.vue";
 import InstallationProgress from "@/components/installation/InstallationProgress.vue";
 import router from "@/router";
+import {translate, TranslationsKey} from "@/plugins/TranslationPlugin";
+import {RouteRecordName} from "vue-router";
 
 const httpService = new HttpService()
 const component: ShallowRef<Component | undefined> = shallowRef(LanguageSetting);
+const installationSteps: RouteRecordName[] = ['language', 'registration', 'privacy', 'sources'];
+
+const nextStep:TranslationsKey = computed(()=>{
+  return `installation.navigation.next.${router.currentRoute.value.name}`
+});
+const lastStep:TranslationsKey = computed(()=>{
+  return `installation.navigation.back.${router.currentRoute.value.name}`
+});
 
 onMounted(() => {
   chooseComponent()
@@ -41,14 +61,40 @@ function chooseComponent() {
   if (routeName === 'privacy') component.value = PrivacySetting
   if (routeName === 'sources') component.value = SourceSetting
 }
+
+function back() {
+  const routeName = router.currentRoute.value.name;
+
+  if (installationSteps.indexOf(routeName) == 0) {
+    return;
+  }
+
+  router.push({name: installationSteps[installationSteps.indexOf(routeName) - 1]});
+}
+function next() {
+  const routeName = router.currentRoute.value.name;
+
+  if (installationSteps.indexOf(routeName) == installationSteps.length - 1) {
+    router.push("/home")
+    return;
+  }
+
+  router.push({name: installationSteps[installationSteps.indexOf(routeName) + 1]});
+}
 </script>
 
 <style scoped>
+nav {
+  background-color: var(--grayish-blue);
+  margin: 0;
+  padding-top: 10px;
+  padding-bottom: 10px;
+}
 
 main {
   background-color: var(--grayish-blue);
   width: 100svw;
-  min-height: 100vh;
+  min-height: calc(100vh - 92px);
   margin: 0;
   padding: 0;
   overflow-y: auto;
@@ -65,13 +111,77 @@ header {
   padding: 25px;
 }
 
+.button-container {
+  display: grid;
+  margin-top: 20px;
+  grid-template-columns: minmax(0, 1fr) 20px minmax(0, 1fr);
+  width: 100%;
+}
+
+.button {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  background-color: var(--grayish-blue);
+  border-radius: var(--border-radius-medium);
+  padding: 5px 0 5px 0;
+
+  user-select: none;
+
+  div {
+    width: 100%;
+    text-align: center;
+
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
+
+    padding: 0 10px 0 10px;
+  }
+
+  img {
+    height: 35px;
+    aspect-ratio: 1;
+  }
+}
+
+.button:hover {
+  cursor: pointer;
+}
+
+.back {
+  grid-column: 1;
+  flex-direction: row-reverse;
+}
+
+.next {
+  grid-column: 3;
+}
+
+.full-width{
+  grid-column: 1 / 4;
+}
+
 @media screen and (min-width: 600px) {
+  nav {
+    background-color: transparent;
+    width: 600px;
+    margin: 50px auto 10px auto;
+  }
+
   main {
     width: 600px;
     border-radius: var(--border-radius-big);
-    margin: 50px auto 0 auto;
-    min-height: calc(100vh - 100px);
-    max-height: calc(100vh - 100px);
+    margin: 0 auto;
+    min-height: calc(100vh - 270px);
+    max-height: calc(100vh - 270px);
+  }
+
+  .button-container {
+    width: 600px;
+    margin: 20px auto 0 auto;
   }
 }
 </style>
