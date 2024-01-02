0b49d9062438e42a69c12ac11d8c3b017ecd0ca7
Daniel <9132@htl.rennweg.at>
2024-01-02 21:39:01 +0100

    added registration to localStorage

---
+++ frontend/src/store/registration.ts
@@ -0,0 +1,42 @@
+import {registration} from "@/store/store";
+
+export type Registration = {
+    username: string,
+    email: string,
+    password: string,
+    timestamp: number,
+    state: "input" | "validation"
+}
+
+export function getRegistration(): Registration {
+// Retrieve registration from localStorage
+    const storedRegistration = localStorage.getItem("registration");
+
+    if (storedRegistration !== null) {
+        const parsedRegistration = JSON.parse(storedRegistration);
+
+        // Check if the parsedRegistration match the Registration type
+        if (isRegistration(parsedRegistration)) {
+            return parsedRegistration;
+        } else {
+            throw new Error("Invalid registration found in localStorage.")
+        }
+    } else {
+        throw new Error("Invalid registration found in localStorage.");
+    }
+}
+
+// Type guard function to check if an object is of type Registration
+function isRegistration(obj: any): obj is Registration {
+    const requiredKeys = Object.keys(registration);
+
+    return (
+        typeof obj === "object" &&
+        obj !== null &&
+        requiredKeys.every((key) => key in obj)
+    );
+}
+
+export function removeRegistration(){
+    localStorage.removeItem("registration")
+}
--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -3,6 +3,8 @@ import type {Settings} from "@/store/settings";
 import {getSettings} from "@/store/settings";
 import type {Installation} from "@/store/installation";
 import {getInstallation} from "@/store/installation";
+import type {Registration} from "@/store/registration";
+import {getRegistration} from "@/store/registration";
 
 export let settings: Settings = reactive({
     language: "en"
@@ -10,16 +12,25 @@ export let settings: Settings = reactive({
 export let installation: Installation = reactive({
     currentStep: "language"
 });
+export let registration: Registration = reactive({
+    username: "",
+    email: "",
+    password: "",
+    timestamp: 0,
+    state: "input"
+});
 
 try {
     settings = reactive(getSettings());
     installation = reactive(getInstallation());
+    registration = reactive(getRegistration());
 } catch (error) {
     console.error(error);
 
     // using default settings
     localStorage.setItem("settings", JSON.stringify(settings));
     localStorage.setItem("installation", JSON.stringify(installation));
+    localStorage.setItem("registration", JSON.stringify(registration));
 }
 
 watch(settings, (newValue) => {
@@ -27,4 +38,7 @@ watch(settings, (newValue) => {
 });
 watch(installation, (newValue) => {
     localStorage.setItem("installation", JSON.stringify(newValue));
+});
+watch(registration, (newValue) => {
+    localStorage.setItem("registration", JSON.stringify(newValue));
 });
