5c0e08bd3bf676e84a12e193213c84c6295bd91f
Daniel <9132@htl.rennweg.at>
2024-01-08 11:07:44 +0100

    added unused import

--- frontend/env.d.ts
+++ frontend/env.d.ts
@@ -1,4 +1,5 @@
 /// <reference types="vite/client" />
+import type {TranslationsKey} from "@/plugins/TranslationPlugin";
 
 declare module "@vue/runtime-core" {
     interface ComponentCustomProperties {
